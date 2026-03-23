34f4e376bee43a95d35522bc268a6744b081133a
GiulioP027 <giulio.paraboni@stud.unilu.ch>
2026-03-23 15:27:36 +0100

    pulizia e tentativo di unione dati

--- data_preprocessed/data_api_trav.R
+++ data_preprocessed/data_api_trav.R
@@ -6,41 +6,134 @@ library(readr)
 library(tidyr)
 library(ggplot2)
 library(tidygeocoder)
+library(stringr)
+library(janitor)
 
 # dati----
+# 
+# raw <- fromJSON("data_raw/hotel_df.json")
+# 
+# ds <- raw$dataset
+# 
+# years <- names(ds$dimension$Jahr$category$index)
+# months <- names(ds$dimension$Monat$category$index)
+# gemeinden <- names(ds$dimension$Gemeinde$category$index)
+# origin <- names(ds$dimension$Herkunftsland$category$index)
+# indikator <- names(ds$dimension$Indikator$category$index)
+# 
+# df_hotellerie <- expand.grid(
+#   Jahr = years,
+#   Monat = months,
+#   Gemeinde = gemeinden,
+#   Herkunftsland = origin,
+#   Indikator = indikator,
+#   KEEP.OUT.ATTRS = FALSE,
+#   stringsAsFactors = FALSE
+# )
+# 
+# 
+# df_hotellerie$value <- ds$value
+# 
+# 
+# df_hotellerie <- df_hotellerie %>%
+#   mutate(
+#     year = as.integer(Jahr),
+#     month = Monat,
+#     month_label = ds$dimension$Monat$category$label[Monat],
+#     municipality_id = as.integer(Gemeinde),
+#     municipality = ds$dimension$Gemeinde$category$label[Gemeinde],
+#     indicator = ds$dimension$Indikator$category$label[Indikator]
+#   )
 
-df_overnight <- read_delim("data_raw/px-x-1003020000_101_20260319-153528.csv", 
-                           delim = ";", escape_double = FALSE, trim_ws = TRUE)
-View(df_overnight) |> print()
 
-df_hotellerie <- read_delim("data_raw/px-x-1003020000_201_20260319-154720_hotellerie.csv", 
-                            delim = ";", escape_double = FALSE, trim_ws = TRUE)
-View(df_hotellerie)
-
-# pulizia dati----
+# tentativo df completo ----
 
 
+load("data_preprocessed/df_analysis.rda")  
+
+# df_analysis ----
+
+df_analysis <- df_analisi
 
 
+# 2) EXPLODE LOCALITIES
 
+df_localities <- df_analysis %>%
+  select(name, localities) %>%
+  mutate(localities = str_split(localities, ";")) %>%
+  unnest(localities) %>%
+  mutate(localities = str_trim(localities))
 
+# funzione pulizia nomi
+clean_name <- function(x) {
+  x %>%
+    str_to_lower() %>%
+    str_replace_all("\\s*\\([^\\)]+\\)", "") %>%  # toglie (BE), (VS), ecc.
+    str_replace_all("[-/]", " ") %>%
+    str_replace_all("[[:punct:]]", " ") %>%
+    str_squish()
+}
 
-df_Chski <- df_Chski %>%
-  reverse_geocode(
-    lat = lat,
-    long = lng,
-    method = "osm",
-    address = municipality
-  )
+df_localities <- df_localities %>%
+  mutate(locality_key = clean_name(localities))
 
-df_Chski <- df_Chski %>%
+# hotel_df----
+
+raw_hotel <- fromJSON("data_raw/hotel_df.json")
+ds1 <- raw_hotel$dataset
+
+df_hotel <- expand.grid(
+  Jahr = names(ds1$dimension$Jahr$category$index),
+  Monat = names(ds1$dimension$Monat$category$index),
+  Gemeinde = names(ds1$dimension$Gemeinde$category$index),
+  Herkunftsland = names(ds1$dimension$Herkunftsland$category$index),
+  Indikator = names(ds1$dimension$Indikator$category$index),
+  KEEP.OUT.ATTRS = FALSE,
+  stringsAsFactors = FALSE
+) %>%
   mutate(
-    municipality = sapply(strsplit(municipality, ","), `[`, 1)
+    value = ds1$value,
+    year = as.integer(Jahr),
+    month = Monat,
+    municipality = unname(ds1$dimension$Gemeinde$category$label[Gemeinde]),
+    indicator = unname(ds1$dimension$Indikator$category$label[Indikator]),
+    municipality_key = clean_name(municipality)
+  ) %>%
+  select(year, month, municipality, municipality_key, indicator, value)
+
+# tieni solo le località che ti servono
+df_hotel_needed <- df_hotel %>%
+  semi_join(
+    df_localities %>% distinct(locality_key),
+    by = c("municipality_key" = "locality_key")
   )
 
+# turism_df----
 
+raw_turism <- fromJSON("data_raw/turism_df.json")
+ds2 <- raw_turism$dataset
 
-print(df_analisi)
-# URL API
-url <- "https://www.pxweb.bfs.admin.ch/api/v1/en/px-x-1003020000_101/px-x-1003020000_101.px"
+df_turism <- expand.grid(
+  Jahr = names(ds2$dimension$Jahr$category$index),
+  Monat = names(ds2$dimension$Monat$category$index),
+  Gemeinde = names(ds2$dimension$Gemeinde$category$index),
+  Indikator = names(ds2$dimension$Indikator$category$index),
+  KEEP.OUT.ATTRS = FALSE,
+  stringsAsFactors = FALSE
+) %>%
+  mutate(
+    value = ds2$value,
+    year = as.integer(Jahr),
+    month = Monat,
+    municipality = unname(ds2$dimension$Gemeinde$category$label[Gemeinde]),
+    indicator = unname(ds2$dimension$Indikator$category$label[Indikator]),
+    municipality_key = clean_name(municipality)
+  ) %>%
+  select(year, month, municipality, municipality_key, indicator, value)
 
+# tieni solo le località che ti servono
+df_turism_needed <- df_turism %>%
+  semi_join(
+    df_localities %>% distinct(locality_key),
+    by = c("municipality_key" = "locality_key")
+  )
